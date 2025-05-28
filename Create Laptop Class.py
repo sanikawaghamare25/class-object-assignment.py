9728a668fc3ee682e2c48a509f0b4f16704d3228
sanika waghamare <sanikawaghamare42@gmail.com>
2025-05-28 15:20:51 +0530

    Create Create Laptop Class.py

---
+++ Create Laptop Class.py
@@ -0,0 +1,18 @@
+'''
+3. Write a python program to Create Laptop Class
+'''
+
+class Laptop:
+Laptop_name="Dell"
+processor="11th Gen intel Core i3"
+RAM="8 GB"
+System_Type="64-bit operating system"
+Windiws= 11
+def diplay(self):
+print("Laptop_name:",self.Laptop_name)
+print("processor:",self.processor)
+print("RAM:",self.RAM)
+print("System_Type:",self.System_Type)
+print("Windiws:",self.Windiws)
+e= Laptop()
+e.diplay()
