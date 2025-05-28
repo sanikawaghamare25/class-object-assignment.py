45f814ad3db59319625d5011d597d7d991d01950
sanika waghamare <sanikawaghamare42@gmail.com>
2025-05-28 15:21:27 +0530

    Create Create shapes Class.py

---
+++ Create shapes Class.py
@@ -0,0 +1,13 @@
+'''
+4. Write a python program to Create shapes Class
+'''
+class Shapes:
+Rectangle="Area=lenght*breadth"
+Square="side*side"
+Triangle="Area=1/2*Base*height"
+def disply(self):
+print("Rectangle:",self.Rectangle)
+print("Square:",self.Square)
+print("Triangle:",self.Triangle)
+e=Shapes()
+e.disply()
