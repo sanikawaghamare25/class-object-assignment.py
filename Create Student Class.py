018c7b944cf041c590185a5629b9ed0b01f67c87
sanika waghamare <sanikawaghamare42@gmail.com>
2025-05-28 15:17:27 +0530

    Create Create Student Class.py

---
+++ Create Student Class.py
@@ -0,0 +1,16 @@
+'''
+1. Write a python program to Create Student Class
+'''
+class Student:
+studRollNo=34
+stuName="sanika"
+studBranch="EXTC"
+stud_college="S S Jondhale college of enginnering"
+def display(self):
+print("studentRolll_No:",self.studRollNo)
+print("student_Name:",self.stuName)
+print("Student_Branch:",self.studBranch)
+print("Student_college:",self.stud_college)
+e= Student()
+
+e.display()
